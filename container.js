061943ae03fd0caf0b87056a270c184cb986aca4
JARED SCARR <JAREDSCARR@GMAIL.COM>
2017-01-19 15:32:26 -0800

    All comments and console.logs removed.

--- container.js
+++ container.js
@@ -1,13 +1,11 @@
 function getImpressions() {
         products = document.querySelectorAll('[data-product-name]');
-        console.log(products);
         groupImpressions(products);
     }
 
 function groupImpressions(productList) {
     var group = [];
     for (var i = 0; i < productList.length; i++) {
-        // map the object from the list to the impression
         var impression = {
             'name': productList[i].getAttribute('data-product-name'),
             'id': productList[i].getAttribute('data-product-id'),
@@ -17,9 +15,7 @@ function groupImpressions(productList) {
             'list': productList[i].getAttribute('data-product-list'),
             'position': i.toString()
         }
-        //adds the impression to the group
         group.push(impression);
-        // call push function (line ) if we have a group of three or no more products
         if (group.length == 3 || i == (productList.length - 1)) {
             pushImpressions(group);
             group = [];
@@ -29,26 +25,19 @@ function groupImpressions(productList) {
 }
 
 function pushImpressions(impressions) {
-    // Check if dataLayer is initialized if not then init it.
     window.dataLayer = window.dataLayer || [];
     var ecommerceObject = {
         'ecommerce': {'impressions': impressions}
     }
-    // here we would actually push the dataLayer
     dataLayer.push(ecommerceObject);
-    console.log('dataLayer is: ', ecommerceObject);
     return ecommerceObject;
 }
 
 function escapeChars(price) {
-    // this is only for the use case specified
-    // could also use regex
     return price.replace('$', '').replace(',', '');
 }
 
 function setVariant(variant) {
-    // checks for 1's and 0's and switches to true or false
-    // could include further checks
     if (variant == 1) {
         return true;
     } else if (variant == 0) {
@@ -58,12 +47,6 @@ function setVariant(variant) {
     }
 }
 
-// If running this on a local server comment out the ga() objects below
-// create tracker object. 
-
 ga('create', '<tracker-id>', 'auto');
-
-// require ecommerce plugin
-
 ga('require', 'ec');
 getImpressions();
